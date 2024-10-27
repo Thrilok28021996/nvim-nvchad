e2d37f19e112ca02b22363d2bd7a34f90b951a97
Thrilok emmadisetty <63156546+Thrilok28021996@users.noreply.github.com>
2024-10-27 20:08:16 +0530

    commented lsp linter

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,74 +1,70 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- EXAMPLE
 local servers = { "html", "cssls", "pylsp", "ruff" }
-local nvlsp = require "nvchad.configs.lspconfig"
+local nvlsp = require("nvchad.configs.lspconfig")
 
-lspconfig.pylsp.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-  settings = {
-    pylsp = {
-      plugins = {
-        jedi_completion = { enabled = false },
-        jedi_hover = { enabled = false },
-        jedi_references = { enabled = false },
-        jedi_signature_help = { enabled = false },
-        jedi_symbols = { enabled = false },
-        pylsp_mypy = { enabled = false },
-        pylsp_black = { enabled = false },
-        pylsp_isort = { enabled = false },
-        pylsp_pycodestyle = { enabled = false },
-        pylsp_pyflakes = { enabled = false },
-        pylsp_mccabe = { enabled = false },
-        -- , mccabe, and pyflakes
-      },
-    },
-  },
-}
-lspconfig.ruff.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-  -- Notes on code actions: https://github.com/astral-sh/ruff-lsp/issues/119#issuecomment-1595628355
-  -- Get isort like behavior: https://github.com/astral-sh/ruff/issues/8926#issuecomment-1834048218
-  commands = {
-    RuffAutofix = {
-      function()
-        vim.lsp.buf.execute_command {
-          command = "ruff.applyAutofix",
-          arguments = {
-            { uri = vim.uri_from_bufnr(0) },
-          },
-        }
-      end,
-      description = "Ruff: Fix all auto-fixable problems",
-    },
-    RuffOrganizeImports = {
-      function()
-        vim.lsp.buf.execute_command {
-          command = "ruff.applyOrganizeImports",
-          arguments = {
-            { uri = vim.uri_from_bufnr(0) },
-          },
-        }
-      end,
-      description = "Ruff: Format imports",
-    },
-  },
-}
+lspconfig.pylsp.setup({
+	on_attach = nvlsp.on_attach,
+	on_init = nvlsp.on_init,
+	capabilities = nvlsp.capabilities,
+	settings = {
+		pylsp = {
+			plugins = {
+				pyflakes = { enabled = false },
+				pycodestyle = { enabled = false },
+				autopep8 = { enabled = false },
+				yapf = { enabled = false },
+				mccabe = { enabled = false },
+				pylsp_mypy = { enabled = false },
+				pylsp_black = { enabled = false },
+				pylsp_isort = { enabled = false },
+			},
+		},
+	},
+})
+lspconfig.ruff.setup({
+	on_attach = nvlsp.on_attach,
+	on_init = nvlsp.on_init,
+	capabilities = nvlsp.capabilities,
+	-- Notes on code actions: https://github.com/astral-sh/ruff-lsp/issues/119#issuecomment-1595628355
+	-- Get isort like behavior: https://github.com/astral-sh/ruff/issues/8926#issuecomment-1834048218
+	commands = {
+		RuffAutofix = {
+			function()
+				vim.lsp.buf.execute_command({
+					command = "ruff.applyAutofix",
+					arguments = {
+						{ uri = vim.uri_from_bufnr(0) },
+					},
+				})
+			end,
+			description = "Ruff: Fix all auto-fixable problems",
+		},
+		RuffOrganizeImports = {
+			function()
+				vim.lsp.buf.execute_command({
+					command = "ruff.applyOrganizeImports",
+					arguments = {
+						{ uri = vim.uri_from_bufnr(0) },
+					},
+				})
+			end,
+			description = "Ruff: Format imports",
+		},
+	},
+})
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = nvlsp.on_attach,
+		on_init = nvlsp.on_init,
+		capabilities = nvlsp.capabilities,
+	})
 end
 
 -- configuring single server, example: typescript
